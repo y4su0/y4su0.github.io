da2f44cb66cd5cd253ea002ceada8dd8a588e966
yasuo ogiuchi <yasuoogi@gmail.com>
2021-07-12 22:55:38 +0900

    theme change

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+function switch_dark_mode_icon(darkmode, id) {
+    darkmode.toggle();
+    render_dark_mode_icon(darkmode, id);
+}
+
+function render_dark_mode_icon(darkmode, id) {
+    var x = document.getElementsByTagName("img");
+    var i;
+
+    console.log(x)
+    if (darkmode.isActivated()) {
+        document.getElementById(id).classList.remove('fa-sun');
+        document.getElementById(id).classList.add('fa-moon');
+        for (i = 0; i < x.length; i++) {
+            x[i].style["mix-blend-mode"] = "difference";
+        }
+
+    } else {
+        document.getElementById(id).classList.remove('fa-moon');
+        document.getElementById(id).classList.add('fa-sun');
+        for (i = 0; i < x.length; i++) {
+            x[i].style["mix-blend-mode"] = "normal";
+        }
+    }
+}
+
--- index.md
+++ index.md
@@ -1,7 +1,22 @@
 ---
-title: y4su0.github.io
+layout: page
+full_logo: true
+title: 
+subtitle: 
+description: A minimal yet feature-rich Jekyll theme made for personal websites and blogs.
+sitemap:
+  priority: 1.0
 ---
+<p id="describe-text">A minimal yet feature-rich Jekyll theme made for personal websites and blogs.</p>
+<br>
+This is the index page, describe yourself in few sentences here. Perhaps talk about what you do for living and what you do in your free time. Maybe even leave an [email@address.com](#) or a link to your [resume](#).
 
-これからいろいろ作る、かもしれない。
+Pudhina Fresh is free and open-source. Checkout [Pudhina Fresh](https://github.com/ritijjain/pudhina-fresh).
 
-- [Jetson Nanoのセットアップ (JetPack 4.5対応)](jetson_nano_jp45.html)
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
